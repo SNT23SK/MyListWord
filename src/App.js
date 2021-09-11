2558888e465c8ff563df6017cc8d55d8295b5220
Константин Скоробогатов <snt23sk@gmail.com>
2021-09-11 20:44:40 +0400

    added input

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
+import Header from "./header/header";
 
 function App() {
+	const [name, setName] = React.useState([]);
 	return (
 		<div className="App">
-			<h1> Hello</h1>
+			<form>
+				<label>Ваше имя</label>
+				<input type="text"></input>
+			</form>
+
+			<Header word="Hello" name={addName}></Header>
 		</div>
 	);
+
+	function addName() {
+		setName(name.concat({ name }));
+	}
 }
 
 export default App;
