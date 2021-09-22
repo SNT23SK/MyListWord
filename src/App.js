6e2f380fadc5280f6d2eed36d641b520be80c885
Константин Скоробогатов <snt23sk@gmail.com>
2021-09-22 15:12:17 +0400

    redisign and added  input

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
 	];
 	return (
 		<div className="App">
-			<h1> Привествия </h1>
+			<h1> Words </h1>
 			<AddWord />
 			<List cards={cards} />
 		</div>
--- src/List/AddWord.js
+++ src/List/AddWord.js
@@ -10,12 +10,12 @@ function useInputValue(defaultValue = "") {
 }
 
 function AddWord() {
-	const input = useInputValue();
-
+	const inputEng = useInputValue();
+	const inputRus = useInputValue();
 	return (
-		<form onSubmit={console.log("clicl button")}>
-			<input {...input.bind}></input>
-
+		<form onSubmit={console.log("click button")}>
+			<input {...inputEng.bind}></input>
+			<input {...inputRus.bind}></input>
 			<button type="submit"> Add word</button>
 		</form>
 	);
