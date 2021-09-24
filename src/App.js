6f4a47287eda1633caffdd3bee368d0f37de6100
Константин Скоробогатов <snt23sk@gmail.com>
2021-09-24 08:28:17 +0400

    added addCards

--- src/App.js
+++ src/App.js
@@ -5,14 +5,25 @@ import AddWord from "./List/AddWord";
 import List from "./List/List";
 
 function App() {
-	const cards = [
+	const [cards, setCards] = React.useState([
 		{ id: 1, eng: "Hello", rus: "Привет" },
 		{ id: 2, eng: "World", rus: "МИР" },
-	];
+	]);
 
 	function addWord(eng, rus) {
 		console.log("Boom", eng, rus);
+
+		setCards(
+			cards.concat([
+				{
+					id: Date.now(),
+					eng,
+					rus,
+				},
+			])
+		);
 	}
+
 	return (
 		<div className="App">
 			<h1> Words </h1>
